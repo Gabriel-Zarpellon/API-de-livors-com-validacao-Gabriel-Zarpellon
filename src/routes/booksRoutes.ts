e3a9455fb3e5970c723be5a6a1f4dfd83d6c6849
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-08-15 13:00:35 -0300

    feat: add validation middleware to routes

--- src/routes/booksRoutes.ts
+++ src/routes/booksRoutes.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { BooksControllers } from "../controllers/books.controllers";
 import { IsBookNameValid } from "../middlewares/isBookNameValid.middleware";
 import { IsBookIdValid } from "../middlewares/isBookIdValid.middleware";
+import { ValidateRequest } from "../middlewares/validateRequest.middleware";
+import { addBookBodySchema, updateBookBodySchema} from "../schemas/books.schema";
 
 export const booksRouter = Router();
 
 const booksControllers = new BooksControllers();
 
-booksRouter.post("/", IsBookNameValid.execute, booksControllers.addBook);
+booksRouter.post("/", ValidateRequest.execute({body: addBookBodySchema}), IsBookNameValid.execute, booksControllers.addBook);
 booksRouter.get("/", booksControllers.getBooks);
 booksRouter.get("/:id", IsBookIdValid.execute, booksControllers.getBook);
-booksRouter.patch("/:id", IsBookIdValid.execute, IsBookNameValid.execute, booksControllers.updateBook);
+booksRouter.patch("/:id", IsBookIdValid.execute, ValidateRequest.execute({body: updateBookBodySchema}), IsBookNameValid.execute, booksControllers.updateBook);
 booksRouter.delete("/:id", IsBookIdValid.execute, booksControllers.deleteBook);
